ae72073a2f9f019135dcb89581570f3c2f3920d2
Kimberly Scaldaferro Colodeti <103203864+KimberlyScaldaC@users.noreply.github.com>
2025-03-17 08:17:48 -0300

    Update manager.cs

--- manager.cs
+++ manager.cs
@@ -29,25 +29,28 @@ public class manager : MonoBehaviour
         // vetGameObj[3].transform.RotateAround(transform.position, Vector3.forward, 5f);
 
         //Posicionar as piramides
-        vetGameObj[4].transform.position = new Vector3(2f, 0f, 0f); //Magenta
-        vetGameObj[5].transform.position = new Vector3(1.5f, 0.87f, 0.29f); //Magenta
-        vetGameObj[6].transform.position = new Vector3(1f, 1.73f, 0.57f); //Magenta
-        vetGameObj[7].transform.position = new Vector3(1.5f, 0f, 0.86f); //Amarelo
-        vetGameObj[8].transform.position = new Vector3(1f, 0f, 1.73f); //Amarelo
-        vetGameObj[9].transform.position = new Vector3(1f, 0.86f, 1.15f); //Amarelo
-        vetGameObj[10].transform.position = new Vector3(0.5f, 0f, 0.86f); //Vermelho
+        //vetGameObj[0].transform.position = new Vector3(0f, 0f, 0f); //Magenta BAIXO DIREITO
+        //vetGameObj[1].transform.position = new Vector3(1f, 0f, 0f); //Magenta BAIXO MEIO
+        vetGameObj[4].transform.position = new Vector3(2f, 0f, 0f); //Magenta BAIXO ESQUERDO
+        //vetGameObj[2].transform.position = new Vector3(0.5f, -0.13397f, 0.28868f); //Magenta MEIO ESQUERDO
+        vetGameObj[5].transform.position = new Vector3(1.5f, 0.87f, 0.29f); //Magenta MEIO DIREITO
+        vetGameObj[6].transform.position = new Vector3(1f, 1.732f, 0.578f); //Magenta CIMA
+        vetGameObj[7].transform.position = new Vector3(1.5f, 0f, 0.86f); //Amarelo BAIXO MEIO
+        vetGameObj[8].transform.position = new Vector3(1f, 0f, 1.73f); //Amarelo BAIXO ESQUEDO
+        vetGameObj[9].transform.position = new Vector3(1f, 0.86f, 1.15f); //Amarelo MEIO ESQUERDO
+        vetGameObj[10].transform.position = new Vector3(0.5f, 0f, 0.86f); //Vermelho BAIXO MEIO
         vetGameObj[11].transform.position = new Vector3(2.5f, 0f, 0.87f); //Baixo
         vetGameObj[12].transform.position = new Vector3(1.5f, 0f, 0.86f); //Baixo
         vetGameObj[13].transform.position = new Vector3(2f, 0f, 1.73f); //Baixo
-        vetGameObj[14].transform.position = new Vector3(1.5f, 0.866f, 0.289f); //Magenta
-        vetGameObj[15].transform.position = new Vector3(2.5f, 0.866f, 0.289f); //Magenta
-        vetGameObj[16].transform.position = new Vector3(2f, 1.732f, 0.578f); //Magenta
-        vetGameObj[17].transform.position = new Vector3(1.656f, 0.338f, 0.374f); //Amarelo
-        vetGameObj[18].transform.position = new Vector3(1.154f, 0.33f, 1.23f); //Amarelo
-        vetGameObj[19].transform.position = new Vector3(1.146f, 1.209f, 0.654f); //Amarelo
-        vetGameObj[20].transform.position = new Vector3(1.507945f, 0.8542254f, 2.008367f); //Vermelho
-        vetGameObj[21].transform.position = new Vector3(0f, 0f, -11f); //Vermelho
-        vetGameObj[22].transform.position = new Vector3(0f, 0f, -14f); //Vermelho
+        vetGameObj[14].transform.position = new Vector3(1.5f, 0.866f, 0.289f); //Magenta Inverso Direito
+        vetGameObj[15].transform.position = new Vector3(2.5f, 0.866f, 0.289f); //Magenta Inverso Esquerdo
+        vetGameObj[16].transform.position = new Vector3(2f, 1.732f, 0.578f); //Magenta Inverso Cima
+        vetGameObj[17].transform.position = new Vector3(1.656f, 0.338f, 0.374f); //Amarelo Inverso Direito
+        vetGameObj[18].transform.position = new Vector3(1.154f, 0.33f, 1.23f); //Amarelo Inverso Esquerdo
+        vetGameObj[19].transform.position = new Vector3(1.152f, 1.199f, 0.658f); //Amarelo Inverso Cima
+        vetGameObj[20].transform.position = new Vector3(1.507945f, 0.8542254f, 2.008367f); //Vermelho Inverso Direito
+        vetGameObj[21].transform.position = new Vector3(1.019f, 0.853f, 1.159f); //Vermelho Inverso Esquerdo
+        vetGameObj[22].transform.position = new Vector3(1.513f, 1.725f, 1.432f); //Vermelho Inverso Cima
 
         //ROTAÇOES
         vetGameObj[11].transform.rotation = Quaternion.Euler(0f, 180f, 0f); //Baixo
