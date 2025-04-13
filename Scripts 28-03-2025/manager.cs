6f21fe94434a0762a78e13e441370cb1e87f21e5
Joaomoreira22 <jv8971641@gmail.com>
2025-04-13 13:56:42 -0300

    manager.cs

--- Scripts 28-03-2025/manager.cs
+++ Scripts 28-03-2025/manager.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class manager : MonoBehaviour
 {
+    private Vector3 p0 = new Vector3(0, 0, 0);
+    private Vector3 p1 = new Vector3(1, 0, 0);
+    private Vector3 p2 = new Vector3(0.5f, 0, Mathf.Sqrt(0.75f));
+    private Vector3 p3 = new Vector3(0.5f, Mathf.Sqrt(0.75f), Mathf.Sqrt(0.75f) / 3);
+
+    public Vector3 eixoVertical, eixoHorizontal, eixoParalelo, eixoParalelo2;
 
     public GameObject tetrahedron; // prefab da camrera
-    public GameObject[] vetGameObj = new GameObject[24];
+    public GameObject[] vetGameObj = new GameObject[22];
     //GameObject pai;
-    GameObject vetorRotPlano1;
-    GameObject vetorRotPlano2;
-    GameObject vetorRotPlano3;
-    Vector3 m_Center;
-    double CP1x, CP1y, CP1z;
-    double CP2x, CP2y, CP2z;
-    double CP3x, CP3y, CP3z;
-    double dx, dy, dz;
+    //GameObject vetorRotPlano1;
+    //
+    GameObject basePivot, basePivot2, basePivot3, basePivot4;
+    //ameObject vetorRotPlano2;
+    //GameObject vetorRotPlano3;
+   //Vector3 m_Center;
+    float CP1x, CP1y, CP1z, CP2x, CP2y, CP2z, CP3x, CP3y, CP3z;
+   
+   // double dx, dy, dz;
     // Use this for initialization
     void Start()
     {
+
         for (int i = 0; i < 24; i++)
         {
             if (i == 0)
@@ -41,10 +51,10 @@ public class manager : MonoBehaviour
         vetGameObj[4].transform.position = new Vector3(2f, 0f, 0f); //Magenta BAIXO ESQUERDO
         //vetGameObj[2].transform.position = new Vector3(0.5f, -0.13397f, 0.28868f); //Magenta MEIO ESQUERDO
         vetGameObj[5].transform.position = new Vector3(1.5f, 0.87f, 0.29f); //Magenta MEIO DIREITO
-        vetGameObj[6].transform.position = new Vector3(1f, 1.732f, 0.578f); //Magenta CIMA
+        vetGameObj[6].transform.position = new Vector3(1f, 1.7321f, 0.578f); //Magenta CIMA
         vetGameObj[7].transform.position = new Vector3(1.5f, 0f, 0.86f); //Amarelo BAIXO MEIO
         vetGameObj[8].transform.position = new Vector3(1f, 0f, 1.73f); //Amarelo BAIXO ESQUEDO
-        vetGameObj[9].transform.position = new Vector3(1f, 0.86f, 1.15f); //Amarelo MEIO ESQUERDO
+        vetGameObj[9].transform.position = new Vector3(1f, 0.86603f, 1.15f); //Amarelo MEIO ESQUERDO
         vetGameObj[10].transform.position = new Vector3(0.5f, 0f, 0.86f); //Vermelho BAIXO MEIO
         vetGameObj[11].transform.position = new Vector3(2.5f, 0f, 0.87f); //Baixo
         vetGameObj[12].transform.position = new Vector3(1.5f, 0f, 0.86f); //Baixo
@@ -74,37 +84,141 @@ public class manager : MonoBehaviour
         vetGameObj[22].transform.rotation = Quaternion.Euler(-162.086f, 55.098f, 34.017f); //Vermelho
 
 
+         CP1x = (0f + 3f + 1.5f) / 3f;
+         CP1y = (0.432f + 0.432f + 0.432f) / 3f;
+         CP1z = (0f + 0f + 2.595f) / 3f;
+
+         CP2x = (0.5f + 2.5f + 1.5f) / 3;
+         CP2y = (1.296f + 1.296f + 1.296f) / 3;
+         CP2z = (0.29f + 0.29f + 2.014f) / 3;
+        
+        //centro do tetraedro DE PLANO 3
+        CP3x = (1f + 2f + 1.5f) / 3;
+        CP3y = (2.163f + 2.163f + 2.163f) / 3;
+        CP3z = (0.578f + 0.578f + 1.442f) / 3;
+
+
+        Vector3 centroBase = new Vector3(CP1x, CP1y, CP1z);
+        Vector3 centroBase2 = new Vector3(CP2x,CP2y, CP2z);
+        Vector3 centroBase3 = new Vector3(CP3x, CP3y, CP3z);
+        float Cx = (p0.x + p1.x + p2.x + p3.x) / 4;
+        float Cy = (p0.y + p1.y + p2.y + p3.y) / 4;
+        float Cz = (p0.z + p1.z + p2.z + p3.z) / 4;
+        Vector3 temp = new Vector3(Cx, Cy, Cz);
+        Vector3 temp1 = new Vector3(Cx, Cy, Cz);
+        Vector3 temp2 = new Vector3(Cx, Cy, Cz);
+        Vector3 temp3 = new Vector3(Cx, Cy, Cz); 
+        
+        temp -= p3;
+        temp1 -= p0;
+        temp2-= p2;
+        temp3 -= p1;
+        eixoHorizontal = temp.normalized;
+        eixoVertical = temp1.normalized;
+        eixoParalelo = temp2.normalized;
+        eixoParalelo2 = temp3.normalized;
+        // Passo 3: Criar um GameObject no centro da base
+        basePivot = new GameObject("BasePivot");
+        basePivot.transform.position = centroBase;
+       
+        basePivot2 = new GameObject("BasePivot2");
+        basePivot2.transform.position = centroBase2;
+
+        basePivot3 = new GameObject("BasePivot3");
+        basePivot3.transform.position = centroBase3;
+        basePivot3 = new GameObject("BasePivot3");
+        basePivot3.transform.position = p0;
+
+        List<GameObject> tetrasNaBase = new List<GameObject>();
+        List<GameObject> tetrasNaBase2 = new List<GameObject>();
+        List<GameObject> tetrasNaBase3 = new List<GameObject>();
+        //List<GameObject> tetrasNaBase3 = new List<GameObject>();
+
+        for (int i = 0; i < vetGameObj.Length; i++)
+        {
+        float y = vetGameObj[i].transform.position.y;
+        float z= vetGameObj[i].transform.position.z;
+
+            // Tolerância pequena para evitar problemas de precisão
+           
+                if(Mathf.Abs(y) <= 0.866f)
+                {
+                    tetrasNaBase.Add(vetGameObj[i]);
+                }
+                //Debug.Log("Tetra na base (Y=0): " + vetGameObj[i].name);
+            
+        }
+        for (int i = 0; i < tetrasNaBase.Count; i++)
+        {
+            GameObject tetra = tetrasNaBase[i];
+
+            tetra.transform.SetParent(basePivot.transform);
+
+            // Muda cor para vermelho
+           // tetra.GetComponent<Renderer>().material.color = Color.red;
+        }
+        for (int i = 0; i < vetGameObj.Length; i++)
+        {
+            float y2 = vetGameObj[i].transform.position.y;
+
+            // Tolerância pequena para evitar problemas de precisão
+            if (y2>= 0.86603f && y2 <= 1.7320f)
+            {
+                tetrasNaBase2.Add(vetGameObj[i]);
+                
+                //Debug.Log("Tetra na base (Y=0): " + vetGameObj[i].name);
+            }
+        }
+        for (int i = 0; i < tetrasNaBase2.Count; i++)
+        {
+            GameObject tetra = tetrasNaBase2[i];
+            tetra.transform.SetParent(basePivot2.transform);
+
+            // Muda cor para vermelho
+            // tetra.GetComponent<Renderer>().material.color = Color.red;
+        }
+        for (int i = 0; i < vetGameObj.Length; i++)
+        {
+            float y2 = vetGameObj[i].transform.position.y;
+
+            // Tolerância pequena para evitar problemas de precisão
+            if (y2 == 0)
+            {
+                tetrasNaBase2.Add(vetGameObj[i]);
+
+                //Debug.Log("Tetra na base (Y=0): " + vetGameObj[i].name);
+            }
+        }
+        for (int i = 0; i < tetrasNaBase3.Count; i++)
+        {
+            GameObject tetra = tetrasNaBase3[i];
+            tetra.transform.SetParent(basePivot4.transform);
+
+            // Muda cor para vermelho
+            }
 
 
 
         // METADE DO TAMANHO DE UM TETRAEDRO = 0.432
 
-        // pai = new GameObject();
-        //pai.transform.position = new Vector3(0,1,0); //pivo
-        // pai.transform.position = new Vector3(0, 1, 0); //pivo
+        vetGameObj[6].transform.SetParent(basePivot3.transform); //pivo
         //vetGameObj[3].transform.parent = pai.transform;
         //vetGameObj[3].transform.bounds
 
-        //centro do tetraedro DE PLANO 1 ENCONTRA O CENTRO DA BASE
+        /*centro do tetraedro DE PLANO 1 ENCONTRA O CENTRO DA BASE
         CP1x = (0 + 3 + 1.5) / 3;
         CP1y = (0.432 + 0.432 + 0.432) / 3;
         CP1z = (0 + 0 + 2.595) / 3;
 
-        //centro do tetraedro DE PLANO 2
-        CP2x = (0.5 + 2.5 + 1.5) / 3;
-        CP2y = (1.296 + 1.296 + 1.296) / 3;
-        CP2z = (0.29 + 0.29 + 2.014) / 3;
+       
 
-        //centro do tetraedro DE PLANO 3
-        CP3x = (1 + 2 + 1.5) / 3;
-        CP3y = (2.163 + 2.163 + 2.163) / 3;
-        CP3z = (0.578 + 0.578 + 1.442) / 3;
+        
 
 
         dx = 1.5;
         dy = 2.595;
         dz = 0.864;
-        /*/VETOR DA ROTACAO DO PLANO 1
+        /*VETOR DA ROTACAO DO PLANO 1
         vetorRotPlano1 = new GameObject();
         GameObject vetorRotPlano1 = new GameObject(x, y, z);
         vetorRotPlano1.transform.position = (CP1x - dx; CP1y - dy; CP1z - dz);
@@ -113,7 +227,8 @@ public class manager : MonoBehaviour
         GameObject vetorRotPlano2 = new GameObject(CP2x - dx, CP2y - dy, CP2z - dz);
 
         vetorRotPlano3 = new GameObject();
-        GameObject vetorRotPlano3 = new GameObject(CP3x - dx, CP3y - dy, CP3z - dz);
+        GameObject vetorRotPlano3 = new GameObject(CP3x - dx, CP3y - dy, CP3z - dz);*/
+        /*
 
 
 
@@ -136,11 +251,41 @@ public class manager : MonoBehaviour
         //Cz = (Az + Bz + Cz + Dz) / 4
         //Cz = (0 + 0 + 1.73 + 0.578) / 4;
     }
+    /*void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+        {
+            other.transform.SetParent(basePivot.transform);
+        }
+
+    }*/
+    
 
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            basePivot.transform.Rotate(eixoHorizontal, 120f, Space.World);
+            //basePivot.transform.Rotate(Vector3.up * 120);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //basePivot2.transform.Rotate(Vector3.up * 120);
+            basePivot2.transform.Rotate(eixoParalelo, 120f, Space.World);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            basePivot3.transform.Rotate(eixoVertical, 120f, Space.World);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            basePivot4.transform.Rotate(eixoParalelo2, 120f, Space.World);
+
+        }
+
         //vetGameObj[3].transform.RotateAround(transform.position, Vector3.forward, 5f);
         //cria um gameobject: Pai. Tem eixo de rotacao
         //por o objeto como filho deste gameobject
